296992641f4a38b5f2bb344d68b1d27fe7b3b8c5
Mery0011 <nasibajahon1995@gmail.com>
2026-04-27 18:40:29 +0900

    fixing

--- app.js
+++ app.js
@@ -1,43 +1,78 @@
-console.log("Web Serverni boshlash");
+console.log('Web Serverni boshlash');
 const express = require("express");
-const app = express();
+const app = express(); // expressning app Objectni yuboradi
 const fs = require("fs");
-
-//MongoDB chaqirish
-const db = require("./server").db();
 
 let user;
 fs.readFile("database/user.json", "utf8", (err, data) => {
-  if(err) {
-    console.log("ERROR:", err);
-  } else {
-    user = JSON.parse(data)
-  }
+    if(err) {
+        console.log("ERROR:" ,err);
+    } else {
+        user = JSON.parse(data)
+    }
 });
 
-//1 Kirish code
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
 
-//2: Session code
-//3: Views code
+// MongoDB choqirish
+const db = require("./server").db();
+
+
+// app objectida 4 ta bosgichga bolamiz
+
+// 1: Kirish code
+app.use(express.static("public")); // public folder ochik boladi
+//clientlarga/koroladi
+app.use(express.json()); // kirip kelyatgan json formatdaki datani
+//Object holatiga bizga ogurip beradi
+app.use(express.urlencoded({extended: true})); // html'dan traditional
+//request form shakli bor, formdan biror narsani post qilsak, bizning
+//express serverimiz qabul qiloladi
+
+// 2: Sesssion code
+
+// 3: Views code
+// bssr - yoli tanladik, backend'de view yasaymiz
+// npm i ejs -> install qilamiz, EJS = Embedded Javascript, ejs orqali
+//backend'da frontend'ni yosaymiz
 app.set("views", "views");
 app.set("view engine", "ejs");
 
-//4 Routing code
-app.post("/create-item", (req, res) => {
-    console.log(req.body);
-    res.json({ test: "success" });
+
+// 4: Routing code
+
+app.post('/create-item', (req, res) => {
+    console.log("user entered /");
+    //console.log(req.body);
+    const new_reja = req.body.reja;
+    db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
+        if(err) {
+            console.log(err);
+            res.end("something went wrong");
+        } else {
+            res.end("successfully added");
+        }
+    });
+    //console.log(req);
+    //res.json({ test: "success" });
 });
 
-app.get("/author", (req, res) => {
-    res.render("authors", {user: user});
+app.get('/author', (req, res) => {
+    res.render('author', {user: user});
+});
+
+app.get("/", function (req, res) {
+    console.log("user entered /");
+    db.collection("plans").find().toArray((err, data) => { // mongoDB
+//bilan CRUD operatsiyani amalga oshiramiz
+        if(err) {
+            console.log(err);
+            res.end("something went wrong");
+        } else {
+            res.render("reja", {items: data });
+        }
+    });
 });
 
 
-app.get("/", function(req, res) {
-    res.render("reja");
-});
 
 module.exports = app;
