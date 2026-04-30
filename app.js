a9472b1aafbc10192a8a025084ff8289f5effa27
Mery0011 <nasibajahon1995@gmail.com>
2026-04-30 23:57:14 +0900

    delete

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ fs.readFile("database/user.json", "utf8", (err, data) => {
 
 // MongoDB choqirish
 const db = require("./server").db();
+const mongodb = require("mongodb");
 
 // 1: Kirish code
 app.use(express.static("public")); 
@@ -37,6 +38,18 @@ app.post('/create-item', (req, res) => {
         res.json(data.ops[0]);
     });
 });
+
+app.post("/delete-item", (req, res) => {
+  const id = req.body.id;
+
+  db.collection("plans").deleteOne(
+    { _id: new mongodb.ObjectId(id) },
+    function (err, data) {
+      res.json({ state: "success" });
+    }
+  );
+});
+
 
 app.get('/author', (req, res) => {
     res.render('author', {user: user});
