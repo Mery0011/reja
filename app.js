8471031736727ab8b5bdf8631dcd41341104471f
Mery0011 <nasibajahon1995@gmail.com>
2026-04-30 22:17:41 +0900

    feat: FrontEnd javascriptni qurish

--- app.js
+++ app.js
@@ -34,15 +34,8 @@ app.post('/create-item', (req, res) => {
     console.log("user entered /");
     const new_reja = req.body.reja;
     db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
-        if(err) {
-            console.log(err);
-            res.end("something went wrong");
-        } else {
-            res.end("successfully added");
-        }
+        res.json(data.ops[0]);
     });
-    //console.log(req);
-    //res.json({ test: "success" });
 });
 
 app.get('/author', (req, res) => {
